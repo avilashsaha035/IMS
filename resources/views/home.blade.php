a8d055c7cf5fb586361fa7c7716f94486e168076
avilashsaha035 <sahaavilash5055@gmail.com>
2023-11-30 01:28:40 +0600

    brush up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,53 +16,34 @@
 
                     {{-- {{ __('You are logged in!') }} --}}
 
-                    {{-- <form action="{{ route('product.insert') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        <div class="mb-3">
-                          <label for="exampleInputEmail1" class="form-label">Name</label>
-                          <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
-                        <div class="mb-3">
-                          <label for="exampleInputPassword1" class="form-label">Quantity</label>
-                          <input type="number" name="quantity" class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Price</label>
-                            <input type="number" name="price" class="form-control" id="exampleInputPassword1">
-                        </div>
-                        <button type="submit" class="btn btn-primary">Add</button>
-                    </form> --}}
-
                     <div class="col-lg-3"></div>
-                    <a href="{{ url('/') }}" class="btn btn-success float-end">Add Product <i class="las la-plus"></i></a>
+                        <a href="{{ url('/') }}" class="btn btn-success float-end">Add Product <i class="las la-plus"></i></a>
                             <div class="cl-lg-6">
                                 <table class="table table-hover" style="border-style:solid; border-color: black;">
                                     <thead>
                                       <tr>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Quantity</th>
-                                        <th scope="col">Price</th>
-                                        <th scope="col">Action</th>
+                                        <th scope="col" class="text-center">Name</th>
+                                        <th scope="col" class="text-center">Quantity</th>
+                                        <th scope="col" class="text-center">Price</th>
+                                        <th scope="col" class="text-center">Action</th>
                                       </tr>
                                     </thead>
                                     <tbody>
                                         @foreach ($products as $item)
                                         <tr>
-                                            <td>{{ $item->name }}</td>
-                                            <td>{{ $item->quantity }}</td>
-                                            <td>{{ $item->price }}</td>
-                                            <td>
+                                            <td class="text-center">{{ $item->name }}</td>
+                                            <td class="text-center">{{ $item->quantity }}</td>
+                                            <td class="text-center">{{ $item->price }}</td>
+                                            <td class="text-center">
                                                 <a href="{{ url('/product/edit/'.$item->id) }}" type="button" class="btn btn-success"> <i class="las la-pen"></i> </a>
                                                 <a href="{{ url('/product/delete/'.$item->id) }}" type="button" class="btn btn-danger"> <i class="las la-trash"></i> </a>
                                             </td>
-                                            {{-- <td type="button" class="btn btn-success"> <i class="las la-pen"></i> </td> --}}
-                                            {{-- <td type="button" class="btn btn-danger"> <i class="las la-trash"></i> </td> --}}
                                         </tr>
                                         @endforeach
                                     </tbody>
                                 </table>
                             </div>
-                        <div class="col-lg-3"></div>
+                    <div class="col-lg-3"></div>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,82 +1,72 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h1 class="text-center text-success fw-bold">Add Products</h1>
+                </div>
+                <div class="card-body">
+                    @if (Route::has('login'))
+                        @auth
 
-{{-- <!DOCTYPE html>
-
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
-        <style>
-
-        </style>
-    </head> --}}
-    {{-- <body class="antialiased"> --}}
-
-        {{-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen  bg-center selection:text-white"> --}}
-            @if (Route::has('login'))
-                {{-- <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10"> --}}
-                    @auth
-                    {{-- <section> --}}
-                        <h1 class="text-center text-success fw-bold">Add Products</h1>
-                        <form action="{{ route('product.insert') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="mb-3">
-                              <label for="exampleInputEmail1" class="form-label">Name</label>
-                              <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                              <span class="text-danger">
-                                @error('name')
-                                    {{ $message }}
-                                @enderror
-                              </span>
-                            </div>
-                            <div class="mb-3">
-                              <label for="exampleInputPassword1" class="form-label">Quantity</label>
-                              <input type="number" name="quantity" class="form-control" id="exampleInputPassword1">
-                              <span class="text-danger">
-                                @error('quantity')
-                                    {{ $message }}
-                                @enderror
-                              </span>
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword1" class="form-label">Price</label>
-                                <input type="number" name="price" class="form-control" id="exampleInputPassword1">
+                            <form action="{{ route('product.insert') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Name</label>
+                                <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                                 <span class="text-danger">
-                                    @error('price')
+                                    @error('name')
                                         {{ $message }}
                                     @enderror
                                 </span>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Add</button>
-                        </form>
+                                </div>
+                                <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Quantity</label>
+                                <input type="number" name="quantity" class="form-control" id="exampleInputPassword1">
+                                <span class="text-danger">
+                                    @error('quantity')
+                                        {{ $message }}
+                                    @enderror
+                                </span>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword1" class="form-label">Price</label>
+                                    <input type="number" name="price" class="form-control" id="exampleInputPassword1">
+                                    <span class="text-danger">
+                                        @error('price')
+                                            {{ $message }}
+                                        @enderror
+                                    </span>
+                                </div>
+                                <button type="submit" class="btn btn-success">Add</button>
+                            </form>
 
-                    {{-- </section> --}}
-                    @else
-                        <a href="{{ route('login') }}" class="btn btn-success font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="btn btn-primary ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                {{-- </div> --}}
-            @endif
+                        @else
+                            <a href="{{ route('login') }}" class="btn btn-success font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="btn btn-primary ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                            @endif
+                        @endauth
+
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
-        {{-- </div> --}}
+
+
+
 
 
 
 @endsection
 
-    {{-- </body>
-</html> --}}
+
